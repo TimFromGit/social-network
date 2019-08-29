b84dd77bc6cd6c29f76c001a6d2dac5cfa37bda6
Tim <ti_mur@mail.ru>
2019-08-29 16:16:55 +0300

    leaarning

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>Simple HTML</li>
+            <li>Css</li>
+            <li>JS</li>
+            <li>React</li>
+        </ul>
+    );
+}
+export default Technologies;
