08d8bb8f0c3116e7416cbb319b8d850dc9045b47
Tim <ti_mur@mail.ru>
2019-08-30 13:48:20 +0300

    разбивка по глобальным компонентам

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.designfreelogoonline.com/wp-content/uploads/2016/03/00106-3D-company-logo-design-free-logo-online-Template-04.png" width="90px"></img>
+        </header>
+    )
+}
+export default Header;
