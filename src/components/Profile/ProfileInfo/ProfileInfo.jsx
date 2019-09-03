868f649112ff4ca28c0cc9e2fcdbb2e246e79bf9
Tim <ti_mur@mail.ru>
2019-09-03 18:54:25 +0300

    рефакторинг стилей

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,7 +6,28 @@ const ProfileInfo = () => {
         <div className={s.descr}>
             <div className={s.img}></div>
             <div className={s.descriptionBlock}>
-                ava+description
+                <div className={s.profilePhoto}></div>
+                <div className={s.profileData}>
+                    <div>
+                        <strong>Name:</strong> John
+                    </div>
+                    <div>
+                        <strong>Surname:</strong> Doe
+                    </div>
+                    <div>
+                        <strong>Ages:</strong> 23
+                    </div>
+                    <div>
+                        <strong>Sex:</strong> man
+                    </div>
+                    <div>
+                        <strong>Interests:</strong> books
+                    </div>
+                    <div>
+                        <strong>About me:</strong> asldjalsdkj adsjdaj alksjd
+                    </div>
+
+                </div>
             </div>
         </div>
     );
