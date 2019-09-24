8c574a652c9387d216ef36c3a7c17e42d3ef526c
Tim <ti_mur@mail.ru>
2019-09-24 19:40:37 +0300

    запрос на сервак об авторизованном пользователе. Начало авторизации

--- src/App.js
+++ src/App.js
@@ -10,12 +10,13 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import FriendsContainer from "./components/Friends/FriendsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
     return (
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer />
             <Navbar/>
             <div className="app-wrapper-content">
                 <Route path='/profile/:userId?' render={() => <ProfileContainer />}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
             <div className={s.logo}>
-                <img src="https://www.freepnglogos.com/uploads/white-jack-daniels-holiday-whiskey-logo-png-2.png" alt="Здесь логотип"/>
+                <img src="https://www.freepnglogos.com/uploads/white-jack-daniels-holiday-whiskey-logo-png-2.png"
+                     alt="Здесь логотип"/>
             </div>
-            <div className={s.title}>
-                welcome to Jacky, buddy!
+            <div className={s.welcomBar}>
+                <div className={s.title}>
+                    Welcome to Jacky, buddy!
+                </div>
+                <div className={s.loginBlock}>
+                    {props.isAuth ? props.login
+                        : <NavLink to={'/login'}>Login</NavLink>}
+                </div>
             </div>
         </header>
     )
